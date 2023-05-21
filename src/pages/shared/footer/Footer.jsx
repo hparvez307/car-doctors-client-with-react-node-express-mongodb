e4cbd7eb6f7fb3eb3495395a669d20358197c079
hparvez307 <hparvez307@gmail.com>
2023-05-22 01:09:46 +0600

    update

--- src/pages/shared/footer/Footer.jsx
+++ src/pages/shared/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from '../../../assets//logo.svg'
 const Footer = () => {
     return (
-        <footer className="footer mt-20 p-10 bg-base-200 text-base-content">
+        <footer className="footer mt-20 px-10  pt-10 bg-base-200 text-base-content">
             <div>
                 <img src={logo} alt="" />
                 <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
